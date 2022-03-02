f06532483dcc94e580195c59303f55b1afded5a4
mnosel00 <mateusz.nosel@microsoft.wsei.edu.pl>
2022-03-02 08:35:20 +0100

    Dodaj pliki projektów.

---
+++ lab1-02.03/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace lab1_02._03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+
+}
