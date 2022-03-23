8adb28ccbcbfdced87de712091cc3e5ae0634468
mnosel00 <mateusz.nosel@microsoft.wsei.edu.pl>
2022-03-23 08:40:04 +0100

    pokaz na lekcji koniec

--- lab3-23.03/Program.cs
+++ lab3-23.03/Program.cs
@@ -25,6 +25,19 @@ namespace lab3_23._03
             stack.Push(10);
             Stack<string> stack2 = new Stack<string>();
             stack2.Push("aaa");
+
+            ValueTuple<string, decimal, int> product = ValueTuple.Create("laptop", 1200m, 2);
+            (string, decimal, int) tuple = product;
+            tuple = ("telefon", 1500m, 4);
+            Console.WriteLine(product);
+            Console.WriteLine(product.Item1);
+            Console.WriteLine(tuple);
+
+            var tuple1 = (name: "Mac", price: 1200m, quantity : 2);
+            Console.WriteLine(tuple1);
+
+
+            Console.WriteLine(tuple1==product);
            /* Stack<object> stack3 = new Stack<object>();
             stack3.Push(1);
             stack3.Push("aaa");*/
