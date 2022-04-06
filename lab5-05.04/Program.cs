c094a071e52ce8924ca2acb68f06f492e6686a23
mnosel00 <mateusz.nosel@microsoft.wsei.edu.pl>
2022-04-06 09:31:36 +0200

    1 i 2 done

---
+++ lab5-05.04/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace lab5_05._04
+{
+   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+
+   
+
+
+}
+
