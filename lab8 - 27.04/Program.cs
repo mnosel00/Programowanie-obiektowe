a3d26c4e1efdc80d3988def0a591aac794cb6622
mnosel00 <mateusz.nosel@microsoft.wsei.edu.pl>
2022-04-27 12:48:48 +0200

    zad1

--- lab8 - 27.04/Program.cs
+++ lab8 - 27.04/Program.cs
@@ -1,117 +1,332 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
+using System.Net;
 
-namespace lab8___27._04
+class Program
 {
-    internal class Program
+    public static readonly string CitiesFileUrl = "http://download.geonames.org/export/dump/cities500.zip";
+    public static readonly string CountryFileUrl = "http://download.geonames.org/export/dump/countryInfo.txt";
+    public static readonly string PathToZipFile = Path.Combine(Path.GetTempPath(), "cities500.zip");
+    public static readonly string CountriesTextFile = "countryInfo.txt";
+    public static readonly string CitiesTextFile = "cities500.txt";
+    public static readonly string CountriesPathToTextFile = Path.Combine(Path.GetTempPath(), CountriesTextFile);
+    public static readonly string CitiesPathToTextFile = Path.Combine(Path.GetTempPath(), CitiesTextFile);
+    static IEnumerable<City> cities = LoadCities();
+    static IEnumerable<Country> countries = LoadCountries();
+
+    public static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.WriteLine();
+        int points = 0;
+        points += Test(() =>
         {
-            List<string> names = new List<string>(){ "adam", "ewa", "ola", "karol", "robert" };
-
-            Predicate<string> NamesA = s =>
+            var polishCities = PolishCities(cities, countries);
+            if (polishCities.Count == 3677
+                && polishCities.Any(c => c.Name.Equals("Warsaw"))
+                && polishCities.Any(city => city.Name.Equals("Nowa Słupia") && city.Population == 1422)
+                && polishCities.Any(city => city.Name.Equals("Kraków"))
+                && polishCities.GroupBy(city => city.Name)
+                    .Any(group => group.Key.Equals("Łaziska") && group.Count() == 5)
+                && polishCities.First().Name.Equals("Abramów")
+                && polishCities.Last().Name.Equals("Żywiec")
+               )
             {
-                Console.WriteLine("Predicate " + s);
-                return s.StartsWith("a");
-            };
-         
-
-            IEnumerable<string> result = from name in names where NamesA.Invoke(name) && name.Length==4
-            select name;
-
-            Console.WriteLine("Ewaluacja");
-            Console.WriteLine(string.Join("\n",result));
-
-
-
-            int[] ints = { 4, 6, 3, 6, 8, 2, 8 };
-
-            IEnumerable<int> result2 = from integ in ints where integ%2==0 select integ;
-
-            Console.WriteLine("zadanie 2");
-            Console.WriteLine(string.Join("\n",result2));
-
-
-            List<Student> students = new List<Student>()
-            {
-                new Student(){Name="Ewa",ETCS=45},
-                new Student(){Name="Ewa1",ETCS=23},
-                new Student(){Name="Ewa2",ETCS=67},
-                new Student(){Name="Ewa3",ETCS=34},
-                new Student(){Name="Ewa4",ETCS=15},
-                
-            };
-
-
-            IEnumerable<string> result3 = from student in students where student.ETCS > 20 select student.Name.ToUpper();
-            Console.WriteLine(string.Join(", ",result3));
-
-
-            IEnumerable<(string Name, string)> egzamin = from student in students select (student.Name, student.ETCS > 20 ? "zdał" : "nie zdał");
-            Console.WriteLine(string.Join(", ",egzamin));
-
-            Console.WriteLine();
-
-            IEnumerable<IGrouping<int, object>> groups = from s in students group s by s.ETCS;
-            foreach (var item in groups)
-            {
-                Console.WriteLine(item.Key +" " + string.Join(", ",item));
+                return 1;
             }
 
-            Console.WriteLine();
-
-
-            IEnumerable<IGrouping<int, int>> intGroups = from x in ints group x by x;
-            foreach (var item in intGroups)
-            {
-                Console.WriteLine(item.Key + " " + string.Join(", ",item));
-            }
-
-            Console.WriteLine();
-
-            IEnumerable<(int Key, int)> enumerable = from n in ints group n by n into gr select(gr.Key, gr.Count<int>());
-            foreach (var item in enumerable)
-            {
-                Console.WriteLine(item.Key + ": " + item.Item2);
-            }
-
-            Console.WriteLine();
-            Console.WriteLine("Fluent API");
-
-            students.Where(student => student.ETCS > 20).Select(student => student.Name);
-
-            Console.WriteLine(string.Join(", ", ints.Where(x => x % 2 == 0).Select(x => x)));
-
-            Console.WriteLine();
-
-            Console.WriteLine(ints.Sum());
-            Console.WriteLine(ints.Average());
-            Console.WriteLine();
-
-            Console.WriteLine("Funkcja agregująca");
-
-            string vv = ints.Aggregate("", (accu, item) => accu + item);
-            Console.WriteLine(vv);
-
-            IEnumerable<int> from0to10 = Enumerable.Range(0, 10);
-            Console.WriteLine(string.Join(", ", from0to10));
-            int r = Enumerable.Range(0, 100).Where(n => n % 2 == 0).Sum();
-            Random random = new Random();
-            random.Next(10);
-
-            //wygeneruj tablice 100 liczb od 0-9
-
-           int[] randomints = Enumerable.Range(0, 100).Select(n => random.Next(10)).ToArray();
-            Console.WriteLine(string.Join(", ",randomints));
-
-        }
-        class Student
+            return 0;
+        }, "Zadanie 1: ");
+        points += Test(() =>
         {
-            public string Name { get; set; }
-            public int ETCS { get; set; }
-        }
+            var result = CountryCities(cities, countries);
+            if (result.Count() == 246
+                && result.Any(pair => pair.CountryCode.Equals("PL") && pair.CitiesCount == 3677)
+                && result.Any(pair => pair.CountryCode.Equals("US") && pair.CitiesCount == 21131)
+                && result.First().CitiesCount == 12 && result.First().CountryCode.Equals("AD")
+                && result.Last().CitiesCount == 66 && result.Last().CountryCode.Equals("ZW")
+               )
+            {
+                return 1;
+            }
+
+            return 0;
+        }, "Zadanie 2: ");
+        points += Test(() =>
+        {
+            var result = Capitals(cities, countries);
+            if (result.Count() == 241
+                && result.First().Equals(("Afghanistan", "Kabul", 3043532))
+                && result.Last().Equals(("Zimbabwe", "Harare", 1542813))
+                && result.Contains(("Poland", "Warsaw", 1702139))
+                && result.First() is (string CountryName, string Capital, long CapitalPopulation))
+            {
+                return 1;
+            }
+
+            return 0;
+        }, "Zadanie 3: ");
+        points += Test(() =>
+        {
+            var result = EvenNumbers(7);
+            if (result.Count() == 4
+                && result.All(n => n % 2 == 0)
+               )
+            {
+                return 1;
+            }
+
+            return 0;
+        }, "Zadanie 4: ");
+        points += Test(() =>
+        {
+            var result = RandomNames(7);
+            if (
+                result.Count() == 7
+                && result.All(n => n.Equals("Adam") || n.Equals("Ewa") || n.Equals("Karol"))
+                && result.GroupBy(n => n).Count() == 3
+            )
+            {
+                return 1;
+            }
+
+            return 0;
+        }, "Zadanie 5: ");
+        points += Test(() =>
+        {
+            var result = Ramp("*", 4);
+            if (
+                result.Count() == 4
+                && result.Last().Length == 4 && result.Last().Equals("****")
+                && result.First().Length == 1 && result.First().Equals("*")
+            )
+            {
+                return 1;
+            }
+
+            return 0;
+        }, "Zadanie 6: ");
+        points += Test(() =>
+        {
+            var result = Tree(4);
+            if (
+                result.Count() == 4
+                && result.Last().Length == 4 && result.Last().Equals("****")
+                && result.First().Length == 1 && result.First().Equals("*")
+            )
+            {
+                return 1;
+            }
+
+            return 0;
+        }, "Zadanie 7: ");
+        Console.WriteLine("Suma punktów " + points);
     }
 
-  
+    //Zadanie 1
+    //Zaimplementuja metodę, aby zwracała listę polskich maist posortowanych alfabetycznie
+    public static List<City> PolishCities(IEnumerable<City> cities, IEnumerable<Country> countries)
+    {
+        var sorted = cities.Where(x=>x.CountryCode=="PL").OrderBy(x=>x.Name).ToList();
+        
+        return sorted;
+       
+    }
+
+    //Zadanie 2
+    //Zaimplementuj metodę zwracająca IEnumerable z krotkami z kodem kraju oraz liczbą miast w tym kraju. Nazwy pól kortek poniżej, w sygnaturze metody.
+    //Krotki posortuj wg kodu kraju
+    public static IEnumerable<(string CountryCode, int CitiesCount)> CountryCities(IEnumerable<City> cities,
+        IEnumerable<Country> countries)
+    {
+        throw new NotImplementedException();
+    }
+
+    //Zadanie 3
+    //Zaimplementuj metodę zwracająca IEnumerable z krotkami z nazwą kraju, nazwą jego stolicy oraz populacją stolicy posortowaną alfabetycznie wg nazwy kraju
+    //Postać krotki poniżej w typie zwracanym
+    public static IEnumerable<(string CountryName, string Capital, long CapitalPopulation)> Capitals(
+        IEnumerable<City> cities, IEnumerable<Country> countries)
+    {
+        throw new NotImplementedException();
+    }
+
+    //Zadanie 4
+    //Zaimplementuj metodę aby zwracała IEnumerable z liczbami parzystymi od 0 do max,
+    //Zapisz wyrażenie z użyciem LINQ Fluent Api i klasy Enumerable
+    public static IEnumerable<int> EvenNumbers(int max)
+    {
+        throw new NotImplementedException();
+    }
+
+    //Zadanie 5
+    //Zaimplementuj metodę, aby zwracała IEnumerable z losowym ciągiem składaącym się z imion: Ewa, Karol,Adam.
+    //Imiona mogą się powtarzać, a liczba powinna być równa parametrowi count, które jest zawsze > 2. Ważne aby, każde imię w ciągu wystąpiło przynajmniej raz!
+    //Przykład
+    //dla wywołania RandomNames(5) przykładowy ciąg to: Ewa, Karol, Ewa, Adam, Adam
+    //Zapisz wyrażenie z użyciem LINQ Fluent Api i klasy Enumerable
+    public static IEnumerable<string> RandomNames(int count)
+    {
+        throw new NotImplementedException();
+    }
+
+    //Zadanie 6
+    //Zaiplementuj metodę zwracającą IEnumerable łańcuchów, złożonych z kolejnych powtórzeń łańcucha s od 1 do count.
+    //Przykład
+    //dla wywołania Ramp("A", 4) zwrócona kolekcja powinna zawierać łańcuchy:
+    //"A", "AA", "AAA", "AAAA"
+    public static IEnumerable<string> Ramp(string s, int count)
+    {
+        throw new NotImplementedException();
+    }
+
+    //Zadanie 7
+    //Zaimplementuj metodę, aby zwracała ciąg łańcuchów o tej samej długości, które wyświetlone w kolejnych wierszach
+    //utworzą trójkąt złożony z gwiazdek o wysokości height
+    //Przykład
+    //Wywołanie Tree(4) utworzy cztery poniższe łańcuchy:
+    //"   *   "
+    //"  ***  "
+    //" ***** "
+    //"*******"
+    //Zastosuj wyłącznie LINQ i metody Prepend i Append 
+    public static IEnumerable<string> Tree(int height)
+    {
+        
+        throw new NotImplementedException();
+    }
+
+    public static int Test(Func<int> testedCode, string message)
+    {
+        try
+        {
+            int points = testedCode.Invoke();
+            if (points > 0)
+            {
+                Console.WriteLine(message + points);
+                return points;
+            }
+
+            Console.WriteLine(message + 0);
+            return 0;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(message + 0);
+        }
+
+        return 0;
+    }
+
+
+    public static List<City> LoadCities()
+    {
+        WebClient client = new WebClient();
+        if (!File.Exists(PathToZipFile))
+        {
+            client.DownloadFile(CitiesFileUrl, PathToZipFile);
+            ZipFile.ExtractToDirectory(PathToZipFile, Path.GetTempPath());
+        }
+
+        List<City> cities = new List<City>();
+        Console.WriteLine("Starting loading cities ...");
+        int count = 0;
+        string message = "";
+        CultureInfo specificCulture = CultureInfo.CreateSpecificCulture("en-EN");
+        foreach (var line in File.ReadLines(CitiesPathToTextFile))
+        {
+            string[] tokens = line.Split("\t", StringSplitOptions.TrimEntries);
+            City city = new City(
+                int.Parse(tokens[0]),
+                tokens[1],
+                double.Parse(tokens[4], NumberStyles.Float, specificCulture),
+                double.Parse(tokens[5], NumberStyles.Float, specificCulture),
+                tokens[6].ToCharArray()[0],
+                tokens[7],
+                tokens[8],
+                long.Parse(tokens[14]),
+                tokens[15].Length > 0 ? int.Parse(tokens[15]) : 0,
+                tokens[17]
+            );
+            cities.Add(city);
+            count++;
+            if (count % 1000 == 0)
+            {
+                Console.Write(string.Join("", Enumerable.Repeat("\b", message.Length)));
+                message = $"Loading {count} cities";
+                Console.Write(message);
+            }
+        }
+
+        Console.Write(string.Join("", Enumerable.Repeat("\b", message.Length)));
+        message = $"Loading {count} cities";
+        Console.Write(message);
+        Console.WriteLine();
+        return cities;
+    }
+
+    public static List<Country> LoadCountries()
+    {
+        WebClient client = new WebClient();
+        if (!File.Exists(CountriesPathToTextFile))
+        {
+            client.DownloadFile(CountryFileUrl, CountriesPathToTextFile);
+        }
+
+        List<Country> countries = new List<Country>();
+        Console.WriteLine("Starting loading countries ...");
+        int count = 0;
+        string message = "";
+        CultureInfo specificCulture = CultureInfo.CreateSpecificCulture("en-EN");
+        foreach (var line in File.ReadLines(CountriesPathToTextFile))
+        {
+            if (line.StartsWith("#"))
+            {
+                continue;
+            }
+
+            string[] tokens = line.Split("\t", StringSplitOptions.TrimEntries);
+            Country country = new Country(
+                tokens[0],
+                tokens[1],
+                tokens[2],
+                tokens[3],
+                tokens[4],
+                tokens[5],
+                tokens[6].Length != 0 ? double.Parse(tokens[6], NumberStyles.Float, specificCulture) : 0,
+                tokens[7].Length != 0 ? int.Parse(tokens[7]) : 0,
+                tokens[8],
+                tokens[9],
+                tokens[10],
+                tokens[11],
+                new List<string>(tokens[18].Split(", "))
+            );
+            countries.Add(country);
+            count++;
+            if (count % 10 == 0)
+            {
+                Console.Write(string.Join("", Enumerable.Repeat("\b", message.Length)));
+                message = $"Loading {count} countries";
+                Console.Write(message);
+            }
+        }
+
+        Console.Write(string.Join("", Enumerable.Repeat("\b", message.Length)));
+        message = $"Loading {count} countries";
+        Console.Write(message);
+        Console.WriteLine();
+        return countries;
+    }
 }
+
+
+public record City(int Id, string Name, double Latitude, double Longitude, char FeatureClass, string FeaturedCode,
+    string CountryCode, long Population,
+    int Elevation, string TimeZoneId);
+
+public record Country(string ISOCode, string ISO3Code, string ISONumeric, string Fips, string CountryName,
+    string Capital, double Area, int Population,
+    string Continent, string CurrencyCode, string CurrencyName, string Phone, List<string> Neighbours);
--- testowanie/Program.cs
+++ testowanie/Program.cs
@@ -1,48 +1,118 @@
-﻿using System.Collections.Generic;
-using System;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
-namespace test
+namespace lab8___27._04
 {
-    class Program
+    internal class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
+            List<string> names = new List<string>() { "adam", "ewa", "ola", "karol", "robert" };
 
-
-
-            string[] arr = { "dd", "aa", "cc", "ss", "aa", "dd", "r", "aa" };
-            Console.WriteLine("oblicz liczbe wystapien kazdego z lancuchów"); 
-            Dictionary<string, int> dic = new Dictionary<string, int>();
-
-            foreach (var item in arr)
+            Predicate<string> NamesA = s =>
             {
-                if (dic.ContainsKey(item))
-                {
-                    dic[item]++;
-                }
-                else
-                {
-                    dic.Add(item, 1);
-                }
-            }
+                Console.WriteLine("Predicate " + s);
+                return s.StartsWith("a");
+            };
 
-            foreach (var item in dic)
+
+            IEnumerable<string> result = from name in names
+                                         where NamesA.Invoke(name) && name.Length == 4
+                                         select name;
+
+            Console.WriteLine("Ewaluacja");
+            Console.WriteLine(string.Join("\n", result));
+
+
+
+            int[] ints = { 4, 6, 3, 6, 8, 2, 8 };
+
+            IEnumerable<int> result2 = from integ in ints where integ % 2 == 0 select integ;
+
+            Console.WriteLine("zadanie 2");
+            Console.WriteLine(string.Join("\n", result2));
+
+
+            List<Student> students = new List<Student>()
             {
-                Console.WriteLine(item.Key + " " + item.Value);
-            }
+                new Student(){Name="Ewa",ETCS=45},
+                new Student(){Name="Ewa1",ETCS=23},
+                new Student(){Name="Ewa2",ETCS=67},
+                new Student(){Name="Ewa3",ETCS=34},
+                new Student(){Name="Ewa4",ETCS=15},
+
+            };
+
+
+            IEnumerable<string> result3 = from student in students where student.ETCS > 20 select student.Name.ToUpper();
+            Console.WriteLine(string.Join(", ", result3));
+
+
+            IEnumerable<(string Name, string)> egzamin = from student in students select (student.Name, student.ETCS > 20 ? "zdał" : "nie zdał");
+            Console.WriteLine(string.Join(", ", egzamin));
+
             Console.WriteLine();
-            Console.WriteLine("wyznacz czesc wspolna obu tablic");
-            int[] set1 = { 2, 33, 6, 6, 1, 7, 8, 9, 10 };
-            int[] set2 = { 3, 6, 8, 9, 11, 2, 1, 78 };
-            //wyznacz czesc wspolna obu tablic
-            var intersect = set1.Intersect(set2);
 
-            foreach (var item in intersect)
+            IEnumerable<IGrouping<int, object>> groups = from s in students group s by s.ETCS;
+            foreach (var item in groups)
             {
-                Console.WriteLine(item);
+                Console.WriteLine(item.Key + " " + string.Join(", ", item));
             }
-        }
 
+            Console.WriteLine();
+
+
+            IEnumerable<IGrouping<int, int>> intGroups = from x in ints group x by x;
+            foreach (var item in intGroups)
+            {
+                Console.WriteLine(item.Key + " " + string.Join(", ", item));
+            }
+
+            Console.WriteLine();
+
+            IEnumerable<(int Key, int)> enumerable = from n in ints group n by n into gr select (gr.Key, gr.Count<int>());
+            foreach (var item in enumerable)
+            {
+                Console.WriteLine(item.Key + ": " + item.Item2);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Fluent API");
+
+            students.Where(student => student.ETCS > 20).Select(student => student.Name);
+
+            Console.WriteLine(string.Join(", ", ints.Where(x => x % 2 == 0).Select(x => x)));
+
+            Console.WriteLine();
+
+            Console.WriteLine(ints.Sum());
+            Console.WriteLine(ints.Average());
+            Console.WriteLine();
+
+            Console.WriteLine("Funkcja agregująca");
+
+            string vv = ints.Aggregate("", (accu, item) => accu + item);
+            Console.WriteLine(vv);
+
+            IEnumerable<int> from0to10 = Enumerable.Range(0, 10);
+            Console.WriteLine(string.Join(", ", from0to10));
+            int r = Enumerable.Range(0, 100).Where(n => n % 2 == 0).Sum();
+            Random random = new Random();
+            random.Next(10);
+
+            //wygeneruj tablice 100 liczb od 0-9
+
+            int[] randomints = Enumerable.Range(0, 100).Select(n => random.Next(10)).ToArray();
+            Console.WriteLine(string.Join(", ", randomints));
+
+        }
+        class Student
+        {
+            public string Name { get; set; }
+            public int ETCS { get; set; }
+        }
     }
+
+
 }
