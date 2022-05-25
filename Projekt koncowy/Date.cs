0203b18fa75f3148c3dd82de519826ef9b5aacec
mnosel00 <mateusz.nosel@microsoft.wsei.edu.pl>
2022-05-25 09:41:31 +0200

    Pobieranie danych z API, konwertowanie z JSON na model

---
+++ Projekt koncowy/Date.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt_koncowy
+{
+    /*public class Date
+    {
+
+        public class Root
+        {
+            public List<CryptoModel> Property1 { get; set; }
+        }
+
+        public class CryptoModel
+        {
+            public string id { get; set; }
+            public string symbol { get; set; }
+            public string name { get; set; }
+            public float current_price { get; set; }
+            public int market_cap_rank { get; set; }
+            public float high_24h { get; set; }
+            public float low_24h { get; set; }
+            public float price_change_24h { get; set; }
+            public float ath { get; set; }
+            public DateTime ath_date { get; set; }
+            public DateTime last_updated { get; set; }
+        }
+
+       
+
+    }*/
+
+
+    public class CryptoModel
+    {
+        public string id { get; set; }
+        public string symbol { get; set; }
+        public string name { get; set; }
+        public float current_price { get; set; }
+        public int market_cap_rank { get; set; }
+        public float high_24h { get; set; }
+        public float low_24h { get; set; }
+        public float price_change_24h { get; set; }
+        public float ath { get; set; }
+        public DateTime ath_date { get; set; }
+        public DateTime last_updated { get; set; }
+    }
+}
--- Projekt koncowy/MainWindow.xaml.cs
+++ Projekt koncowy/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,38 +16,75 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Xml.Linq;
+//using Newtonsoft.Json;
+//using static Projekt_koncowy.Data;
 
 namespace Projekt_koncowy
 {
-    record Crypto (string symbol, string name, float current_price, long market_cap, int market_cap_rank, float high_24h, float low_24h, float price_change_24h);
+   // record Crypto (string symbol, string name, float current_price, long market_cap, int market_cap_rank, float high_24h, float low_24h, float price_change_24h);
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        Dictionary<string, Crypto> Rates = new Dictionary<string, Crypto>();
+        //Dictionary<string, Crypto> Rates = new Dictionary<string, Crypto>();
 
         private void DownloadData()
         {
-            WebClient client = new WebClient();
+            /*WebClient client = new WebClient();
 
             client.Headers.Add("Content-Type", "application/json");
             string jsonRates = client.DownloadString("https://api.coingecko.com/api/v3/coins/markets?vs_currency=pln&order=market_cap_desc&per_page=100&page=1&sparkline=false");
             XDocument doc = XDocument.Parse(jsonRates);
 
-            //Zmienic pobranego XML na słownik rekordow Rate
+            //Zmienic pobranego XML na słownik rekordow Rate*/
 
          
         }
         public MainWindow()
         {
             InitializeComponent();
+            //getAllItems();
+            DownloadJson();
+            
+
         }
 
         private void GetAll_Click(object sender, RoutedEventArgs e)
         {
+
+            //getAllItems();
+           // DownloadJson();
+        }
+
+       /* private void getAllItems()
+        {
+            using(WebClient client = new WebClient())
+            {
+                string url = string.Format("https://api.coingecko.com/api/v3/coins/markets?vs_currency=pln&order=market_cap_desc&per_page=100&page=1&sparkline=false");
+                string json = client.DownloadString(url);
+
+               Data.Root info = JsonConvert.DeserializeObject<Data.Root>(json);
+
+                string input = info.CryptoCurrencyModel.name.ToString();
+
+                ListaApi.Text = input;
+
+
+            }
+
+        }*/
+
+        private void DownloadJson()
+        {
+            WebClient client = new WebClient();
+            client.Headers.Add("Content-Type", "application/json");
+            string json = client.DownloadString("https://api.coingecko.com/api/v3/coins/markets?vs_currency=pln&order=market_cap_desc&per_page=100&page=1&sparkline=false");
+            List<CryptoModel> info = JsonSerializer.Deserialize<List<CryptoModel>>(json);
            
-            
+
+           
+
         }
     }
 }
