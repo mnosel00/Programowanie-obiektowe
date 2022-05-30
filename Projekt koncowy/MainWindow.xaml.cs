1603cee62bef341a7dc49e24fc264d24ad030763
mnosel00 <mateusz.nosel@microsoft.wsei.edu.pl>
2022-05-30 18:11:40 +0200

    Dodanie kalkulatora. wymiana walut na krypto (not done)

--- Projekt koncowy/MainWindow.xaml.cs
+++ Projekt koncowy/MainWindow.xaml.cs
@@ -22,40 +22,50 @@ using System.Xml.Linq;
 namespace Projekt_koncowy
 {
     record Crypto(string id, string symbol, string name, float current_price, int market_cap_rank, float high_24h, float low_24h, float price_change_24h, float ath, DateTime ath_date, DateTime last_updated);
+    record Exchange(float result);
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
         Dictionary<string, Crypto> Rates = new Dictionary<string, Crypto>();
-
-        private void DownloadData()
-        {
-            /*WebClient client = new WebClient();
-
-            client.Headers.Add("Content-Type", "application/json");
-            string jsonRates = client.DownloadString("https://api.coingecko.com/api/v3/coins/markets?vs_currency=pln&order=market_cap_desc&per_page=100&page=1&sparkline=false");
-            XDocument doc = XDocument.Parse(jsonRates);
-
-            //Zmienic pobranego XML na słownik rekordow Rate*/
+        Dictionary<int, Exchange> RatesEx = new Dictionary<int, Exchange>();
+        float pieniadz;
 
 
-        }
         public MainWindow()
         {
+            
             InitializeComponent();
+            DropCurencyList.Items.Add("USD");
+            DropCurencyList.Items.Add("PLN");
+            DropCurencyList.Items.Add("EUR");
+            DropCurencyList.Items.Add("GBP");
+
+            Curency.Items.Add("USD");
+            Curency.Items.Add("PLN");
+            Curency.Items.Add("EUR");
+            Curency.Items.Add("GBP");
+
+            DropCurencyList.SelectedIndex = 1;
+
             DownloadJson();
+
             foreach (var item in Rates)
             {
                 DropList.Items.Add(item.Value.name);
+                Coin.Items.Add(item.Value.name);
             }
             DropList.SelectedIndex = 0;
+            Coin.SelectedIndex = 0;
+
 
         }
 
         private void GetAll_Click(object sender, RoutedEventArgs e)
         {
-
+            ListaApi.Text = "";
+            DownloadJson();
 
             foreach (var item in Rates)
             {
@@ -65,37 +75,101 @@ namespace Projekt_koncowy
 
         private void GetFromDropMenu_click(object sender, RoutedEventArgs e)
         {
+            DownloadJson();
             string inputCoin = DropList.Text;
-            // ListFromDropMenu.Text = Rates.Select(x=>(x.Value.name,x.Value.current_price,x.Value.ath,x.Value.ath_date)).Where(x=>x.name==inputCoin).ToString();
+             /*ListFromDropMenu.Text = Rates.Select(x=>(x.Value.name,x.Value.current_price,x.Value.ath,x.Value.ath_date)).Where(x=>x.name==inputCoin).ToString();*/
 
             foreach (var item in Rates)
             {
                 if (item.Value.name==inputCoin)
                 {
-                    ListFromDropMenu.Text = $"Nazwa coina: {item.Key}{"\n"}  Cena: {item.Value.name}  {item.Value.current_price}, Max Wartość: {item.Value.ath}, Data Max Wartość: {item.Value.ath_date} \n\n"; 
+                    //ListFromDropMenu.Text = $"Nazwa coina: {item.Key}{"\n"}  Cena: {item.Value.name}  {item.Value.current_price}, Max Wartość: {item.Value.ath}, Data Max Wartość: {item.Value.ath_date} \n\n";
+                    ListFromDropMenu.Text = item.Value.ToString();
                 }
             }
         }
 
+        private void GetCalc_Click(object sender, RoutedEventArgs e)
+        {
+            DownloadJson();
+
+            int kwota = int.Parse(MoneyAmount.Text);
+            string curency = Curency.Text;
+            string coin = Coin.Text;
+            string symbol = Rates.Where(x => x.Value.name == coin).Select(x => x.Value.symbol).FirstOrDefault();
+            float coinPrice = Rates.Where(x => x.Value.name == coin).Select(x => x.Value.current_price).FirstOrDefault();
+
+            if (DropCurencyList.Text == curency)
+            {
+       
+                CalcList.Text = $"Za kwotę {kwota}{curency} możesz kupić {kwota / coinPrice} {symbol}";
+            }
+            else if (DropCurencyList.Text == "PLN" && DropCurencyList.Text != curency)
+            {
+                DownloadJsonExchange();
+                CalcList.Text = $"Za kwotę {pieniadz}{DropCurencyList.Text} możesz kupić {pieniadz / coinPrice} {symbol}";
+            }
+
+            // API key: xkp4MKt6Cd8IJddRngLS3mQKZHmIIomd
+            //fetch("https://api.apilayer.com/exchangerates_data/convert?to={to}&from={from}&amount={amount}", requestOptions)
+
+        }
+
         private void DownloadJson()
         {
+            string currency = DropCurencyList.Text;
             WebClient client = new WebClient();
             client.Headers.Add("Content-Type", "application/json");
-            string json = client.DownloadString("https://api.coingecko.com/api/v3/coins/markets?vs_currency=pln&order=market_cap_desc&per_page=100&page=1&sparkline=false");
+            string json = client.DownloadString($"https://api.coingecko.com/api/v3/coins/markets?vs_currency={currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false");
             List<CryptoModel> info = JsonSerializer.Deserialize<List<CryptoModel>>(json);
+
+            //https://api.coingecko.com/api/v3/coins/markets?vs_currency=pln&order=market_cap_desc&per_page=100&page=1&sparkline=false
 
 
 
             foreach (var item in info)
             {
-                Rates.Add(item.id, new Crypto(item.id, item.symbol, item.name,
+                if (!Rates.ContainsKey(item.id))
+                {
+                    Rates.Add(item.id, new Crypto(item.id, item.symbol, item.name,
                                                 item.current_price, item.market_cap_rank,
                                                 item.high_24h, item.low_24h, item.price_change_24h,
                                                 item.ath, item.ath_date, item.last_updated
                     ));
+                }
+                else
+                {
+                    
+                    Rates[item.id] = new Crypto(item.id, item.symbol, item.name,
+                                                item.current_price, item.market_cap_rank,
+                                                item.high_24h, item.low_24h, item.price_change_24h,
+                                                item.ath, item.ath_date, item.last_updated);
+                }
+               
+                
             }
 
 
+        }
+        
+        private void DownloadJsonExchange()
+        {
+            string to = DropCurencyList.Text;
+            string from = Curency.Text;
+            string amount = MoneyAmount.Text;
+            int id = 0;
+
+
+            WebClient clientEx = new WebClient();
+            clientEx.Headers.Add("Content-Type", "application/json");
+            string jsn = clientEx.DownloadString($"https://api.apilayer.com/exchangerates_data/convert?to={to}&from={from}&amount={amount}&apikey=xkp4MKt6Cd8IJddRngLS3mQKZHmIIomd");
+            //List<Rootobject> infoEx = JsonSerializer.Deserialize<List<Rootobject>>(jsn);
+
+            Rootobject infoEx = JsonSerializer.Deserialize<Rootobject>(jsn);
+
+            //https://api.apilayer.com/exchangerates_data/convert?to=pln&from=usd&amount=1000&apikey=xkp4MKt6Cd8IJddRngLS3mQKZHmIIomd
+
+            pieniadz = infoEx.result;
         }
 
 
